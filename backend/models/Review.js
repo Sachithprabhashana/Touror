5f981810b084059fafb1760f9c94a69e6ea2249b
sachith <sachithprabhashana@gmail.com>
2023-07-25 21:58:52 +0530

    initial commit with images

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import mongoose from 'mongoose';
 
-export const Review = () => {
-  return (
-    <div>Review</div>
-  )
-}
+const reviewSchema = new mongoose.Schema(
+  {
+    productId:{
+      type: mongoose.Types.ObjectId,
+      ref:"Tour"
+    },
+    username:{
+      type: String,
+      required:true,
+    },
+    reviewText:{
+      type: String,
+      required:true,
+    },
+    rating:{
+      type: Number,
+      required:true,
+      min: 0,
+      max:5,
+      default:0,
+    },
+   
+  },
+  {timestamps:true}
+);
+
+export default mongoose.model('Review', reviewSchema);
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import mongoose from 'mongoose';
 
-export const User = () => {
-  return (
-    <div>User</div>
-  )
-}
+const userSchema = new mongoose.Schema(
+  {
+    username:{
+      type: String,
+      required:true,
+      unique:true,
+    },
+    email:{
+      type: String,
+      required:true,
+      unique:true,
+    },
+    password:{
+      type: String,
+      required:true,
+    },
+    photo:{
+      type: String,
+    },
+    role:{
+      type:String,
+      default: "user",
+    },
+   
+  },
+  {timestamps:true}
+);
+
+export default mongoose.model('User', userSchema);
